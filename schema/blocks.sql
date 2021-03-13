b1ae6c340021171820f8b874ec721aa1589c5fbb
Mukarram Ishaq <mukarramishaq189@gmail.com>
2021-03-13 21:51:35 +0500

    added support for postgres exporter

--- schema/blocks.sql
+++ schema/blocks.sql
@@ -1,17 +1,17 @@
 create table blocks(
+    timestamp timestamp,
+    number bigint,
     hash varchar(66),
+    nonce varchar(200),
+    difficulty numeric(38),
     size bigint,
     stripped_size bigint,
     weight bigint,
-    number bigint,
     version bigint,
     merkle_root varchar(66),
-    timestamp timestamp,
-    nonce bigint,
     bits varchar(500),
     coinbase_param varchar(500),
     transaction_count bigint,
-    difficulty numeric(38),
     previous_block_hash varchar(66),
     type varchar(20)
 );
